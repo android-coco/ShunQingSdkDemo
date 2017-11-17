f79642d6679494468e15f8ed7397a21452bad0e7
youhao <yh_android@163.com>
2017-11-17 11:04:50 +0800

    指定拨号修改

--- SunQingLib/src/main/java/yh/org/shunqinglib/aty/ZdBhActivity.java
+++ SunQingLib/src/main/java/yh/org/shunqinglib/aty/ZdBhActivity.java
@@ -1,19 +1,23 @@
 package yh.org.shunqinglib.aty;
 
+import android.text.InputFilter;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 
 import org.yh.library.okhttp.YHRequestFactory;
 import org.yh.library.okhttp.callback.HttpCallBack;
 import org.yh.library.ui.YHViewInject;
 import org.yh.library.utils.JsonUitl;
 import org.yh.library.utils.StringUtils;
+import org.yh.library.view.loading.dialog.YHLoadingDialog;
 
 import yh.org.shunqinglib.R;
 import yh.org.shunqinglib.base.BaseActiciy;
 import yh.org.shunqinglib.bean.JsonLjDWModel;
 import yh.org.shunqinglib.utils.GlobalUtils;
+import yh.org.shunqinglib.utils.PreferenceHelper;
 
 /**
  * Created by yhlyl on 2017/10/24.
@@ -23,10 +27,14 @@ import yh.org.shunqinglib.utils.GlobalUtils;
 public class ZdBhActivity extends BaseActiciy
 {
     //@BindView(id = R.id.number)
-    private EditText number;
+    // 手机输入框
+    private AutoCompleteTextView number;
     //@BindView(id = R.id.bh, click = true)
     private Button bh;
-
+    private String[] autoStrs = new String[4];
+    private int size = 0;// 当前有多少历史手机号码
+    // 输入的号码
+    String sNumber;
     @Override
     public void setRootView()
     {
@@ -35,10 +43,37 @@ public class ZdBhActivity extends BaseActiciy
     }
 
     private void initView(){
-        number = (EditText) findViewById(R.id.number);
-        bh = (Button) findViewById(R.id.bh);
+        number = (AutoCompleteTextView) findViewById(R.id.phonenum);
+        number.setFilters(new InputFilter[]
+                {new InputFilter.LengthFilter(16)});
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(aty,
+                R.layout.simple_long_report, autoStrs);
+        number.setAdapter(adapter);
+
+
+        bh = (Button) findViewById(R.id.sendlongrep);
         bh.setOnClickListener(this);
     }
+
+    @Override
+    public void initData()
+    {
+        super.initData();
+        for (int i = 0; i < autoStrs.length; i++)
+        {
+            String number = PreferenceHelper.readString(GlobalUtils.user_xml,
+                    "number1" + i);
+            if (StringUtils.isEmpty(number))
+            {
+                number = "";
+            } else
+            {
+                size++;
+            }
+            autoStrs[i] = number;
+        }
+    }
+
     @Override
     public void initWidget()
     {
@@ -59,14 +94,21 @@ public class ZdBhActivity extends BaseActiciy
     {
         super.widgetClick(v);
         int i = v.getId();
-        if (i == R.id.bh)
+        if (i == R.id.sendlongrep)
         {
-            if (StringUtils.isEmpty(number.getText().toString()))
+            sNumber = number.getText().toString();
+            if (!StringUtils.isEmpty(sNumber))
             {
-                YHViewInject.create().showTips("请输入号码！");
+                if (sNumber.length() >= 7 && sNumber.length() <= 16)
+                {
+                    zjBH();
+                } else
+                {
+                    YHViewInject.create().showTips("请输入正确的号码！");
+                }
             } else
             {
-                zjBH();
+                YHViewInject.create().showTips("号码不能为空！");
             }
 
         }
@@ -74,10 +116,11 @@ public class ZdBhActivity extends BaseActiciy
 
     private void zjBH()
     {
+        YHLoadingDialog.make(aty).setMessage("拨号中。。。")//提示消息
+                .setCancelable(false).show();
         YHRequestFactory.getRequestManger().postString(GlobalUtils.HOME_HOST, GlobalUtils
                 .TERMINAL_CALL, null, "{\"sn\":\"" + GlobalUtils.DEIVER_SN + "\",\"number\":\""
-                + number
-                .getText().toString() + "\"}", new
+                + sNumber + "\"}", new
                 HttpCallBack()
                 {
                     @Override
@@ -111,6 +154,25 @@ public class ZdBhActivity extends BaseActiciy
                     public void onFinish()
                     {
                         super.onFinish();
+                        boolean f = true;
+                        for (int i = 0; i < autoStrs.length; i++)
+                        {
+                            if (sNumber.equals(autoStrs[i]))
+                            {
+                                f = false;
+                                break;
+                            }
+                        }
+                        if (f)
+                        {
+                            if (size > 3)
+                            {
+                                size = (size % 4);
+                            }
+                            PreferenceHelper.write(GlobalUtils.user_xml,
+                                    "number1" + size, sNumber);
+                        }
+                        YHLoadingDialog.cancel();
                     }
                 }, TAG);
     }
