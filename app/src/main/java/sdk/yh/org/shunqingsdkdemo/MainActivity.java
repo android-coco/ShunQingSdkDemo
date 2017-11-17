824e562a066e3c673f02c71bb438a4ac50479ad7
youhao <yh_android@163.com>
2017-11-17 11:04:13 +0800

    修改测试SN和正式SN

--- app/src/main/java/sdk/yh/org/shunqingsdkdemo/MainActivity.java
+++ app/src/main/java/sdk/yh/org/shunqingsdkdemo/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends BaseActiciy
         {
             case R.id.ok:
                 GlobalUtils.HOME_HOST = "http://www.shunqing365.net";//接口地址
-                GlobalUtils.DEIVER_SN = "123456789012345";//SN号
+                GlobalUtils.DEIVER_SN = "A100004DC57E0D";//SN号
                 showActivity(aty, LocationByBDActivity.class);
                 break;
             case R.id.ok1:
